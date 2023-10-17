32c7a7e9f82399fa9dd41a81a7bb1480baaf8a93
SriNithiksha <ssritharan@northislandcollege.ca>
2023-10-17 16:34:12 -0700

    Create the html structure in php files and add styles to it

---
+++ gallery.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Holiday Booking</title>
+    <link rel="stylesheet" href="styles.css">
+    <link rel="Destinations" href="destination.php" />
+</head>
+
+<body>
+    <header>
+        <nav>
+            <div class="logo">Bon-Voyage</div>
+            <ul class="nav-links">
+                <li><a href="index.php">Home</a></li>
+                <li><a href="destination.php">Destinations</a></li>
+                <li><a href="packages.php">Packages</a></li>
+                <li><a href="gallery.php">Gallery</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <main>
+        <h1>Gallery</h1>
+    </main>
+    <section class="gallery">
+        <div class="image">
+            <img src="assets/images/destination1.jpg" alt="Image 1">
+        </div>
+        <div class="image">
+            <img src="assets/images/destination2.jpg" alt="Image 2">
+        </div>
+        <div class="image">
+            <img src="assets/images/destination3.jpg" alt="Image 3">
+        </div>
+        <div class="image">
+            <img src="assets/images/destination4.jpg" alt="Image 4">
+        </div>
+        <div class="image">
+            <img src="assets/images/destination5.jpg" alt="Image 5">
+        </div>
+        <div class="image">
+            <img src="assets/images/destination6.jpg" alt="Image 6">
+        </div>
+        <div class="image">
+            <img src="assets/images/destination7.jpg" alt="Image 7">
+        </div>
+        <div class="image">
+            <img src="assets/images/destination8.jpg" alt="Image 8">
+        </div>
+        <div class="image">
+            <img src="assets/images/destination9.jpg" alt="Image 9">
+        </div>
+        <div class="image">
+            <img src="assets/images/destination10.jpg" alt="Image 10">
+        </div>
+        <div class="image">
+            <img src="assets/images/destination11.jpg" alt="Image 11">
+        </div>
+        <div class="image">
+            <img src="assets/images/destination12.jpg" alt="Image 12">
+        </div>
+        <div class="image">
+            <img src="assets/images/destination13.jpg" alt="Image 13">
+        </div>
+        <div class="image">
+            <img src="assets/images/destination14.jpg" alt="Image 14">
+        </div>
+        <div class="image">
+            <img src="assets/images/destination15.jpg" alt="Image 15">
+        </div>
+        <div class="image">
+            <img src="assets/images/destination16.jpg" alt="Image 16">
+        </div>
+    </section>
+        
+
+    <footer>
+        <p>&copy; 2023 Bon-Voyage</p>
+    </footer>
+</body>
+
+</html>
+
+</body>
+
+</html>
